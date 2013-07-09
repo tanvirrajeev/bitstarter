5a9fa1869cb93dbaec8c47128ea5ca3225b40986
Tanvir Rahim <tanvirrajeev@gmail.com>
2013-07-09 10:45:13 +0000

    Move read code under server block

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var express = require('express');
 
+
 debugger;
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World deployed form git');
-});
+
 
 
 //Read data from index.html
@@ -19,6 +21,20 @@ fs.readFile('./index.html', function read(err, data){
 	content = data;
 	console.log(content);
 });
+
+
+});
+
+
+//Read data from index.html
+//var content;
+//fs.readFile('./index.html', function read(err, data){
+//	if (err){
+//		throw err;
+//		}
+//	content = data;
+//	console.log(content);
+//});
 
 
 var port = process.env.PORT || 5000;
