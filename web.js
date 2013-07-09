77e872790be97100c172f90db2e1f3ca1d74ca4b
Tanvir Rahim <tanvirrajeev@gmail.com>
2013-07-09 09:50:07 +0000

    add fs.readSync first time

--- web.js
+++ web.js
@@ -6,6 +6,18 @@ app.get('/', function(request, response) {
   response.send('Hello World deployed form git');
 });
 
+
+//Read data from index.html
+var content;
+fs.readFile('./index.html', function read(err, data){
+	if (err){
+		throw err;
+		}
+	content = data;
+	console.log(content);
+});
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
